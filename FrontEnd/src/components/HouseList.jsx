fe5258b2f0b6ce93607d421f1fd558da2f4890fd
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-07-06 07:19:57 +0100

    House List

--- FrontEnd/src/components/HouseList.jsx
+++ FrontEnd/src/components/HouseList.jsx
@@ -1,7 +1,28 @@
+import { useContext } from 'react'
+import {HouseContext} from './HouseContext'
+
+import House from './House'
+import { Link }  from 'react-router-dom'
+import { ImSpinner2 } from 'react-icons/im'
 
 const HouseList = () => {
+  const { houses, loading } = useContext(HouseContext)
+
+  console.log(houses)
   return (
-    <div>HouseList</div>
+    <section className='mb-20'>
+      <div className="container max-auto">
+        <div>
+          {houses.map((house, index) => {
+            return (
+              <Link to={`/property/${house.id}`} key={index}>
+                <House house={house}/>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
