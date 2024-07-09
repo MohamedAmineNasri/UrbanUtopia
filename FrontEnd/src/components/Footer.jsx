80fa7ec18ca61ef2ff1edfa3a40c61da0c8e8578
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-07-09 23:41:09 +0100

    Footer Section

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-violet-700 py-8 text-center text-white'>
+      <div className="container mx-auto">
+        Copyright &copy; 2024. All Rights Reserved 
+      </div>
+    </footer>
   )
 }
 
