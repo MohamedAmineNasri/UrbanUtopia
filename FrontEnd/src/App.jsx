79f9a3fe90a4e422121bab79358909d3a9e6ada9
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-07-14 14:19:02 +0100

    Logout Button

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
@@ -8,15 +8,15 @@ import PropertyDetails from "./pages/PropertyDetails"
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 const App = () => {
-
+  const user = true
   return (
     <div className='max-w-[1440px] mx-auto bg-white'>
-      <Header /> 
+      <Header user={user}/> 
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/property/:id' element={<PropertyDetails />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/signup' element={<SignUp />}/>
+        <Route path='/login' element={user ? <Navigate to='/'/> : <Login />}/>
+        <Route path='/signup' element={user ? <Navigate to='/'/> :<SignUp />}/>
       </Routes>
       <Footer /> 
     </div>
--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -2,13 +2,19 @@
 import {Link} from 'react-router-dom'
 import Logo from '../assets/img/logo.svg'
 
-const Header = () => {
+const Header = ({user}) => {
   return (
     <header className='py-6 mb-12 border-b'>
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/"><img src={Logo} alt="" /></Link>
         <div className='flex items-center gap-6'>
-          <Link className='hover:text-violet-900 transtion'  to="/login">Log In</Link>
+          {
+            user ? (
+                <Link className='hover:text-violet-900 transtion'  to="/">Log Out</Link>
+            ) : (
+                <Link className='hover:text-violet-900 transtion'  to="/login">Log In</Link>
+            )
+            }
           <Link className='bg-violet-700 
             hover:bg-violet-800 
             text-white px-4 py-3 rounded-lg
